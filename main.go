a8cadb931f14a565568824e106c686bd5690a39f
BoYu Huang <boyuhuang.michael@gmail.com>
2023-03-12 21:16:41 -0500

    Refactor with gorilla and middleware

--- main.go
+++ main.go
@@ -8,16 +8,30 @@ import (
 	"os/signal"
 	"test_run/Go-microservices/handlers"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
+	// create the handlers
 	ph := handlers.NewProducts(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	// create a new serve mux and register the handlers
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
 
 	// hh := handlers.NewHello(l)
 	// sm.Handle("/", hh)
